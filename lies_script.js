24838c75f8df994611bf24c2ba1de1b81aacb96e
Dominic Rout <d.rout@sheffield.ac.uk>
2013-03-06 14:41:16 +0000

    Fixed some typos and syntax error

--- lies_script.js
+++ lies_script.js
@@ -1,5 +1,4 @@
 $(function () {
-	$("body").prepend("<h1><marquee>THESE ARE ALL LIES
-		                               REALLY, DON'T WORRY ABOUT IT</marquee></h1>");
+	$("body").prepend("<h1><marquee>THESE ARE ALL LIES REALLY, DON'T WORRY ABOUT IT</marquee></h1>");
 	$('marquee').marquee();
 });
